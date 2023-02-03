f1f0f11c43d723f1ff25ac3cefd9d6b43431af3f
A.L <xenide@users.noreply.github.com>
2023-02-03 23:50:45 +0800

    script: deploy our dummy tokens

--- scripts/setup_scaffold.s.sol
+++ scripts/setup_scaffold.s.sol
@@ -1,11 +1,14 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
+import { WETH } from "solmate/tokens/WETH.sol";
+
 import "v3-core/scripts/BaseScript.sol";
 import { ConstantProductPair } from "v3-core/src/curve/constant-product/ConstantProductPair.sol";
 import { StableMintBurn } from "v3-core/src/curve/stable/StableMintBurn.sol";
 import { StablePair } from "v3-core/src/curve/stable/StablePair.sol";
 import { FactoryStoreLib } from "v3-core/src/libraries/FactoryStore.sol";
+import { MintableERC20 } from "v3-core/test/__fixtures/MintableERC20.sol";
 
 import { ReservoirRouter } from "src/ReservoirRouter.sol";
 import { Quoter } from "src/Quoter.sol";
@@ -27,13 +30,21 @@ contract SetupScaffold is BaseScript {
     ReservoirRouter private _router;
     Quoter private _quoter;
 
+    MintableERC20 internal _usdc;
+    MintableERC20 internal _usdt;
+    WETH internal _wavax;
+
     // default private key from anvil
     uint256 private _defaultPrivateKey = 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
+    address private _walletAddress;
 
-    function _deployPeriphery() private {
+    function _deployInfra() private {
         vm.startBroadcast(_defaultPrivateKey);
-        _router = new ReservoirRouter(address(_factory), WAVAX_AVAX_MAINNET);
-        _quoter = new Quoter(address(_factory), WAVAX_AVAX_MAINNET);
+        _usdc = new MintableERC20("USD Circle", "USDC", 6);
+        _usdt = new MintableERC20("USD Tether", "USDT", 6);
+        _wavax = new WETH();
+        _wavax.deposit{value: 5_000 ether}();
+        require(_wavax.balanceOf(_walletAddress) == 5_000 ether, "WAVAX AMT WRONG");
         vm.stopBroadcast();
     }
 
@@ -57,7 +68,11 @@ contract SetupScaffold is BaseScript {
         _factory.write("SP::swapFee", DEFAULT_SWAP_FEE_SP);
         _factory.write("SP::amplificationCoefficient", DEFAULT_AMP_COEFF);
 
-        _factory.createPair(WAVAX_AVAX_MAINNET, USDC_AVAX_MAINNET, 0);
+        ConstantProductPair lPair1 = ConstantProductPair(_factory.createPair(address(_usdt), address(_usdc), 0));
+        _usdc.mint(address(lPair1), 1_000_000e6);
+        _usdt.mint(address(lPair1), 950_000e6);
+        lPair1.mint(address(this));
+        require(lPair1.balanceOf(address(this)) > 0, "INSUFFICIENT LIQ");
         // _factory.createPair(USDC_AVAX_MAINNET, USDT_AVAX_MAINNET, 1);
         // _factory.createPair(WAVAX_AVAX_MAINNET, USDC_AVAX_MAINNET, 1);
         vm.stopBroadcast();
@@ -66,7 +81,16 @@ contract SetupScaffold is BaseScript {
         require(lAllPairs.length == 1, "Wrong number of pairs created");
     }
 
+    function _deployPeriphery() private {
+        vm.startBroadcast(_defaultPrivateKey);
+        _router = new ReservoirRouter(address(_factory), WAVAX_AVAX_MAINNET);
+        _quoter = new Quoter(address(_factory), WAVAX_AVAX_MAINNET);
+        vm.stopBroadcast();
+    }
+
     function run() external {
+        _walletAddress = vm.rememberKey(_defaultPrivateKey);
+        _deployInfra();
         _deployCore();
         _deployPeriphery();
     }
