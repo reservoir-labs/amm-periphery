45ca2363754abfb695082abb6dfc970c0b8cc338
A.L <xenide@users.noreply.github.com>
2023-06-14 23:41:33 +0200

    fix: replace OZ's IERC20 impl with forge-std's

--- src/abstract/PeripheryPayments.sol
+++ src/abstract/PeripheryPayments.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/token/ERC20/IERC20.sol";
-
 import "src/interfaces/IPeripheryPayments.sol";
 import "src/interfaces/IWETH.sol";
 
