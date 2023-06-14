d0ce3eea6272bb45419984c75c0a5f767a7d41ba
A.L <xenide@users.noreply.github.com>
2023-06-14 23:09:20 +0200

    lint: forge fmt

--- script/deploy_router_timelock_asset_manager.s.sol
+++ script/deploy_router_timelock_asset_manager.s.sol
@@ -10,7 +10,8 @@ import { AaveManager, IPoolAddressesProvider } from "amm-core/src/asset-manageme
 contract DeployRouterTimeLockAaveManager is Script {
     address internal constant FACTORY = 0x47e537e1452DBc9C3eE8F1420e5aaF22111D3547;
     address internal constant WETH_AVAX_MAINNET = 0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7;
-    IPoolAddressesProvider internal constant AAVE_POOL_ADDRESSES_PROVIDER = IPoolAddressesProvider(0xa97684ead0e402dC232d5A977953DF7ECBaB3CDb);
+    IPoolAddressesProvider internal constant AAVE_POOL_ADDRESSES_PROVIDER =
+        IPoolAddressesProvider(0xa97684ead0e402dC232d5A977953DF7ECBaB3CDb);
 
     function run() external {
         vm.startBroadcast(msg.sender);
