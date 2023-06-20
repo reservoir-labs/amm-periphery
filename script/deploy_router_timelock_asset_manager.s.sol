6bc568ffb7bb4a4533397e7548c9131f53194b44
A.L <xenide@users.noreply.github.com>
2023-06-20 09:34:40 +0200

    fix: update factory address

--- script/deploy_router_timelock_asset_manager.s.sol
+++ script/deploy_router_timelock_asset_manager.s.sol
@@ -8,7 +8,7 @@ import { ReservoirTimelock } from "amm-core/src/ReservoirTimelock.sol";
 import { AaveManager, IPoolAddressesProvider } from "amm-core/src/asset-management/AaveManager.sol";
 
 contract DeployRouterTimeLockAaveManager is Script {
-    address internal constant FACTORY = 0x47e537e1452DBc9C3eE8F1420e5aaF22111D3547;
+    address internal constant FACTORY = 0xDd723D9273642D82c5761a4467fD5265d94a22da;
     address internal constant WETH_AVAX_MAINNET = 0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7;
     IPoolAddressesProvider internal constant AAVE_POOL_ADDRESSES_PROVIDER =
         IPoolAddressesProvider(0xa97684ead0e402dC232d5A977953DF7ECBaB3CDb);
@@ -16,8 +16,8 @@ contract DeployRouterTimeLockAaveManager is Script {
     function run() external {
         vm.startBroadcast(msg.sender);
         _deployRouter();
-        _deployTimelock();
-        _deployAaveManager();
+//        _deployTimelock();
+//        _deployAaveManager();
         vm.stopBroadcast();
     }
 
